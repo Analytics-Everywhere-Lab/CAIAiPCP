2c3f6ec685dd3d429c581e7097e826f00fb29874
Rene Richard <rene.richard@nrc-cnrc.gc.ca>
2025-08-27 20:50:17 -0300

    fixed ability to book display open time slots and book appointment

--- mcp/src/main.py
+++ mcp/src/main.py
@@ -3,6 +3,7 @@ from util.async_db import execute_sql
 from typing import List
 from util import settings
 import logging
+import datetime
 
 
 # Create an MCP server
@@ -12,6 +13,7 @@ mcp = FastMCP("Demo")
 @mcp.tool()
 async def get_provider_names() -> List[dict]:
     """returns a list of available providers"""
+    logging.info('Getting list of providers ....')
     res = await execute_sql("SELECT DISTINCT provider_name, role FROM AVAILABILITY", True)
     providers = [{"provider_name": e["provider_name"], "role": e["role"]} for e in res]
     return providers
@@ -20,34 +22,50 @@ async def get_provider_names() -> List[dict]:
 @mcp.tool()
 async def get_provider_roles() -> List[dict]:
     """returns a list of available provider roles"""
+    logging.info('Getting list of provider roles')
     res = await execute_sql("SELECT DISTINCT role FROM AVAILABILITY", True)
     roles = [{"role": e["role"]} for e in res]
     return roles
 
 
 @mcp.tool()
-async def get_available_slots_by_provider(provider_name: str) -> List[dict]:
-    """returns a list of available / open provider time slots"""
-    res = await execute_sql("SELECT dt_time_slot FROM AVAILABILITY WHERE lower(provider_name) = %s and is_available = True", True, provider_name.lower())
-    roles = [{"available_time_slot": e["dt_time_slot"]} for e in res]
-    return roles
+async def get_available_booking_slots_for_provider(provider_name: str) -> List[dict]:
+    """
+    Returns booking 5 earliest time slots for a provider.
+
+    Output is ALWAYS JSON with fields:
+      - slot_number (integer, sorted ascending)
+      - provider_name (string)
+      - time_slot (string, format 'YYYY-MM-DD HH24:MI:SS')
+    """
+    logging.info(f'Getting open slots for {provider_name}')
+    res = await execute_sql(
+            """
+            SELECT provider_name, slot_number, TO_CHAR(dt_time_slot, 'YYYY-MM-DD HH24:MI:SS') as time_slot FROM AVAILABILITY 
+            WHERE lower(provider_name) = %s 
+            and 
+            is_available = True 
+            order by slot_number asc limit (5)""", 
+            True, provider_name.lower())
+    open_slots = [{"provider_name": e["provider_name"],"slot_number": e["slot_number"], "time_slot": e["time_slot"]} for e in res]
+    return open_slots
 
 
 @mcp.tool()
 async def book_slot_for_provider(provider_name: str, time_slot: str, client_id: str) -> int:
-    """books client appoinment with provider at specified time slot"""
+    """books a client appoinment with a provider at specified time slot. Time slot is expressed as 'YYYY-MM-DD HH24:MI:SS'"""
+    logging.info(f'Booking appointment with : {provider_name} for client : {client_id} on {time_slot}')
     affected = await execute_sql("""
                             UPDATE availability 
                             SET 
-                                patient_to_attend = %s, is_available = False 
+                                client_to_attend = %s, is_available = False 
                             WHERE 
                                 dt_time_slot = TO_TIMESTAMP(%s, 'YYYY-MM-DD HH24:MI:SS')
                             AND
                                 LOWER(provider_name) = %s
                             """, False, client_id, time_slot, provider_name.lower())
     print(f'Rows affected : {affected}')
-    return 1
-    # return affected
+    return affected
 
 
 if __name__ == "__main__":
